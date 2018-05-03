7b4e4faf37e6df77f8c0b1cd72f5405a254d67d7
fortunto2 <info@3dstr.ru>
2018-05-03 15:22:16 +0500

    new

--- twitch.sh
+++ twitch.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 # https://blog.twitch.tv/live-video-transmuxing-transcoding-ffmpeg-vs-twitchtranscoder-part-i-489c1c125f28
+#  -x264params='-x264-params keyint='+keyint+':min-keyint='+min_keyint+':scenecut='+scenecut+':rc-lookahead='+keyint
+# -x264opts keyint=60:no-scenecut \
 
-ffmpeg -i ./data/test.mp4 \
+
+ffmpeg -i ./data/test2.mp4 \
   -c:v libx264 \
-  -x264opts keyint=60:no-scenecut \
+  -x264-params keyint=120:min-keyint=60:scenecut=50:rc-lookahead=300 \
   -s 640x360 \
   -r 30 \
   -b:v 1000k \
@@ -13,4 +16,4 @@ ffmpeg -i ./data/test.mp4 \
   -c:a aac \
   -sws_flags bilinear \
   -hls_playlist_type vod \
-  ./vod2/twitch.m3u8
+  ./vod/twitch.m3u8
